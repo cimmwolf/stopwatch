d1b89a7725ea840385191fcb6ce2515bab7cc45b
cimmwolf <cimmwolf@gmail.com>
2019-08-25 18:12:07 +0400

    Clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,9 @@
+/* global require */
 const gulp = require('gulp');
 const autoprefixer = require('autoprefixer');
 const imagemin = require('gulp-imagemin');
 const cssNano = require('cssnano');
-const polyclean = require('polyclean');
-const htmlmin = require('gulp-htmlmin');
-const cache = require('gulp-cached');
 const postcss = require('gulp-postcss');
-
-gulp.task('default', function() {
-  return gulp.src([
-    'bower_components/app-*/*.html',
-    'bower_components/iron-*/*.html',
-    'bower_components/paper-*/*.html',
-    'bower_components/google-*/*.html',
-    'bower_components/gold-*/*.html',
-    'bower_components/neon-*/*.html',
-    'bower_components/platinum-*/*.html',
-    'bower_components/polymer/*.html',
-    'dist/components/*.html'
-  ], {base: './'})
-      .pipe(cache('components'))
-      .pipe(polyclean.cleanCss())
-      .pipe(polyclean.leftAlignJs())
-      .pipe(polyclean.uglifyJs())
-      .pipe(htmlmin({removeComments: true}))
-      .pipe(gulp.dest('bower_components'));
-});
 
 gulp.task('css', function() {
   gulp.src('style.css')
